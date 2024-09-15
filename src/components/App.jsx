c3432c9e1825aee89fbe8d1bca2e4ab5daa49c5e
Chand-585 <101199856+Chand-585@users.noreply.github.com>
2024-09-15 11:11:23 +0530

    Update App.jsx
    commented the repeated code and commiting the new changes for the contact.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      phone={contact.phone}
+      email={contact.email}
+    />
+  );
+}
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      {contacts.map(createCard)}
+      {/*
       <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
@@ -23,6 +36,7 @@ function App() {
         phone={contacts[2].phone}
         email={contacts[2].email}
       />
+  */}
     </div>
   );
 }
